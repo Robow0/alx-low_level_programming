c4aefedf5773bbc4d2e1bffb296666b8e9a339ec
Robow0 <robowsalad@gmail.com>
2023-06-06 12:16:52 -0700

    updated

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  * print_number - prints a number
  * @n: Input number
@@ -7,11 +6,13 @@
 void print_number(int n)
 {
 	int res, temp, expo;
-
+	
 	expo = 1;
 /*Check negatives*/
 	if (n >= 0)
+	{
 		res = n * -1;
+	}
 	else
 	{
 		res = n;
@@ -24,10 +25,13 @@ void print_number(int n)
 	{
 		expo *= 10;
 		temp /= 10;
+		printf("expo: %d, temp: %d, orig: %d\n", expo, temp, res);
 	}
 /*Main */
 	while (expo >= 1)
 	{
+
+		printf("expo: %d, res: %d\n", expo, ((res /expo) % 10) * -1);
 		_putchar(((res / expo) % 10) * -1 + '0');
 		expo /= 10;
 	}
