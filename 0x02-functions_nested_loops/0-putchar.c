c7a84daee6764f5cb61c7bfe47cfa87e83946b78
Robow0 <robowsalad@gmail.com>
2023-07-19 01:19:22 -0700

    0-putchar.c updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
+
 /**
- * main - main function .
- *
- * Return: 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char h[] = "_putchar";
-	int s = sizeof(h);
-	int i;
-
-	for (i = 0; i < s; i++)
-	{
-		_putchar(h[i]);
-
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
