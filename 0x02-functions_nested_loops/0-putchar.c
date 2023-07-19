4011f226e0fc405ea81ab8b5030d49c5c3ec163c
Robow0 <robowsalad@gmail.com>
2023-07-19 01:00:56 -0700

    _putchar updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
- * main - prints the string "_putchar" from a character array.
- * Return: 0 on success.
+ * main - main function .
+ *
+ * Return: 0
  */
 int main(void)
 {
-	char c[] = "_putchar";
+	char h[] = "0-putchar.c";
+	int s = sizeof(h);
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < s; i++)
 	{
-		_putchar(c[i]);
+		_putchar(h[i]);
+
 	}
+
 	_putchar('\n');
 	return (0);
 }
