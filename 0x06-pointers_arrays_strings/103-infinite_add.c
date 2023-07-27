0c391da8ccaf7ef2a22b8f34ffe030fa0fd58da4
Robow0 <robowsalad@gmail.com>
2023-07-27 04:47:41 -0700

    updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,12 @@
+#include "main.h"
+/**
+ * print_buffer - add 2 strings.
+ * @b: string1.
+ * @size: string2.
+ * Return: String with all letters in ROT13 base.
+ */
+void print_buffer(char *b, int size)
+
 /**
  * infinite_add - Add up two numbers stored in given char arrays
  * @n1: The first number
