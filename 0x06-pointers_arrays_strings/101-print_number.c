df022adf5aa93fcafbec1a7705260b63fb3c5688
Robow0 <robowsalad@gmail.com>
2023-07-27 00:42:01 -0700

    updated

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,39 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * print_number - prints a number
- * @n: Input number
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c)
 
-void print_number(int n)
 {
-	int res, temp, expo;
-	
-	expo = 1;
-/*Check negatives*/
-	if (n >= 0)
-	{
-		res = n * -1;
-	}
-	else
-	{
-		res = n;
-		_putchar('-');
-	}
-
-/*Initialize exponent variable*/
-	temp = res;
-	while (temp <= -10)
-	{
-		expo *= 10;
-		temp /= 10;
-		printf("expo: %d, temp: %d, orig: %d\n", expo, temp, res);
-	}
-/*Main */
-	while (expo >= 1)
-	{
-
-		printf("expo: %d, res: %d\n", expo, ((res /expo) % 10) * -1);
-		_putchar(((res / expo) % 10) * -1 + '0');
-		expo /= 10;
-	}
+  return (write(1, &c, 1));
 }
