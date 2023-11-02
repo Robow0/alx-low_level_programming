de74399377f0114ae911000ddbdb74ce32385b78
Robow0 <robowsalad@gmail.com>
2023-11-02 05:22:23 -0700

    updated

--- 0x14-bit_manipulation/100-main.c
+++ 0x14-bit_manipulation/100-main.c
@@ -3,16 +3,16 @@
 
 int main(void)
 {
-int n;
+    int n;
 
-n = get_endianness();
-if (n != 0)
-	{
-		printf("Little Endian\n");
-	}
-else
-	{
-		printf("Big Endian\n");
-	}
-	return (0);
+    n = get_endianness;
+    if (n != 0)
+    {
+        printf("Little Endian\n");
+    }
+    else
+    {
+        printf("Big Endian\n");
+    }
+    return (0);
 }
